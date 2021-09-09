c1f0a3cb3add99a154c984b8f4a2ae9a863c120f
lucas.farias <lucas.farias@db1.com.br>
2021-09-09 16:00:21 -0300

    :sparkles: Create method to call theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { ThemesService } from './services/themes.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'custom-theme';
+  title = 'custom-dynamic-theme';
+
+  constructor(private themeService: ThemesService) {}
+
+  ngOnInit(): void {
+    this.themeService.loadTheme(environment.availableTheme);
+  }
+
+  changeTheme() {
+    console.log('teste');
+
+    this.themeService.loadTheme('secondary');
+  }
 }
